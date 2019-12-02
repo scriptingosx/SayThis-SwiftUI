71a967b022045dbdbd0e3277844d2d22218b221d
Armin Briegel <ab@scriptingosx.com>
2019-12-02 14:45:11 +0100

    Initial Commit

---
+++ SayThis/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SayThis
+//
+//  Created by Armin Briegel on 02.12.19.
+//  Copyright © 2019 Scripting OS X. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
